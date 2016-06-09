1d349b55678760b04e3404ffa434849fe5169716
Martijn Straatman <M.Straatman-1@student.tudelft.nl>
2016-06-09 15:04:02 +0200

    Add intersection triangle, not working yet

--- src/raytracing.cpp
+++ src/raytracing.cpp
@@ -8,8 +8,8 @@
 
 
 //temporary variables
-//these are only used to illustrate 
-//a simple debug drawing. A ray 
+//these are only used to illustrate
+//a simple debug drawing. A ray
 Vec3Df testRayOrigin;
 Vec3Df testRayDestination;
 
@@ -22,10 +22,10 @@ void init()
 {
 	//load the mesh file
 	//please realize that not all OBJ files will successfully load.
-	//Nonetheless, if they come from Blender, they should, if they 
+	//Nonetheless, if they come from Blender, they should, if they
 	//are exported as WavefrontOBJ.
 	//PLEASE ADAPT THE LINE BELOW TO THE FULL PATH OF THE dodgeColorTest.obj
-	//model, e.g., "C:/temp/myData/GraphicsIsFun/dodgeColorTest.obj", 
+	//model, e.g., "C:/temp/myData/GraphicsIsFun/dodgeColorTest.obj",
 	//otherwise the application will not load properly
     MyMesh.loadMesh("models/dodgeColorTest.obj", true);
 	MyMesh.computeVertexNormals();
@@ -45,7 +45,76 @@ Vec3Df performRayTracing(const Vec3Df & origin, const Vec3Df & dest)
 	return Vec3Df(dest[0],dest[1],dest[2]);
 }
 
+Vec3Df trace(const Vec3Df & origin, const Vec3Df & dest)
+{
+    printf("begin");
+    for(int i=0; i<MyMesh.triangles.size(); ++i)
+        {
+            Triangle triangle = MyMesh.triangles[i];
+            if(calculateHit(origin, dest, triangle))
+                printf("calculateHit: true");
+        }
+}
 
+bool calculateHit(const Vec3Df & origin, const Vec3Df & dest, const Triangle & triangle)
+{
+    Vec3Df v0 = MyMesh.vertices[triangle.v[0]].p;
+    Vec3Df v1 = MyMesh.vertices[triangle.v[1]].p;
+    Vec3Df v2 = MyMesh.vertices[triangle.v[2]].p;
+
+    // compute plane's normal
+    Vec3Df v0v1 = v1 - v0;
+    Vec3Df v0v2 = v2 - v0;
+    // no need to normalize
+    Vec3Df N = Vec3Df::crossProduct(v0v2, v0v1); // N
+    float area2 = N.getLength();
+    float t;
+    float u;
+    float v;
+
+// Step 1: finding P
+
+// check if ray and plane are parallel ?
+    float NdotRayDirection = Vec3Df::dotProduct(dest, N);
+    if (fabs(NdotRayDirection) < 0.0000001) // almost 0
+    return false; // they are parallel so they don't intersect !
+
+// compute d parameter using equation 2
+    float d = Vec3Df::dotProduct(v0, N);
+
+// compute t (equation 3)
+    t = (Vec3Df::dotProduct(origin, N) + d) / NdotRayDirection;
+// check if the triangle is in behind the ray
+    if (t < 0) return false; // the triangle is behind
+
+// compute the intersection point using equation 1
+    Vec3Df P = origin + t * dest;
+
+// Step 2: inside-outside test
+    Vec3Df C; // vector perpendicular to triangle's plane
+
+// edge 0
+    Vec3Df edge0 = v1 - v0;
+    Vec3Df vp0 = P - v0;
+    C = Vec3Df::crossProduct(vp0, edge0);
+    if (Vec3Df::dotProduct(C, N) < 0) return false; // P is on the right side
+
+// edge 1
+    Vec3Df edge1 = v2 - v1;
+    Vec3Df vp1 = P - v1;
+    C = Vec3Df::crossProduct(vp1, edge1);
+    u = C.getLength() / area2;
+    if (Vec3Df::dotProduct(C, N) < 0) return false; // P is on the right side
+
+// edge 2
+    Vec3Df edge2 = v0 - v2;
+    Vec3Df vp2 = P - v2;
+    C = Vec3Df::crossProduct(vp2, edge2);
+    v = C.getLength() / area2;
+    if (Vec3Df::dotProduct(C, N) < 0) return false; // P is on the right side;
+
+return true; // this ray hits the triangle
+}
 
 void yourDebugDraw()
 {
@@ -54,7 +123,7 @@ void yourDebugDraw()
 
 	//let's draw the mesh
 	MyMesh.draw();
-	
+
 	//let's draw the lights in the scene as points
 	glPushAttrib(GL_ALL_ATTRIB_BITS); //store all GL attributes
 	glDisable(GL_LIGHTING);
@@ -65,9 +134,9 @@ void yourDebugDraw()
 		glVertex3fv(MyLightPositions[i].pointer());
 	glEnd();
 	glPopAttrib();//restore all GL attributes
-	//The Attrib commands maintain the state. 
+	//The Attrib commands maintain the state.
 	//e.g., even though inside the two calls, we set
-	//the color to white, it will be reset to the previous 
+	//the color to white, it will be reset to the previous
 	//state after the pop.
 
 	for (std::vector<BoundingBox>::iterator it = boxes.begin(); it != boxes.end(); ++it) {
@@ -88,12 +157,12 @@ void yourDebugDraw()
 	glVertex3fv(MyLightPositions[0].pointer());
 	glEnd();
 	glPopAttrib();
-	
+
 	//draw whatever else you want...
 	////glutSolidSphere(1,10,10);
 	////allows you to draw a sphere at the origin.
 	////using a glTranslate, it can be shifted to whereever you want
-	////if you produce a sphere renderer, this 
+	////if you produce a sphere renderer, this
 	////triangulated sphere is nice for the preview
 }
 
@@ -118,17 +187,17 @@ void drawBox(BoundingBox box) {
 //x,y is the mouse position in pixels
 //rayOrigin, rayDestination is the ray that is going in the view direction UNDERNEATH your mouse position.
 //
-//A few keys are already reserved: 
+//A few keys are already reserved:
 //'L' adds a light positioned at the camera location to the MyLightPositions vector
-//'l' modifies the last added light to the current 
+//'l' modifies the last added light to the current
 //    camera position (by default, there is only one light, so move it with l)
-//    ATTENTION These lights do NOT affect the real-time rendering. 
+//    ATTENTION These lights do NOT affect the real-time rendering.
 //    You should use them for the raytracing.
-//'r' calls the function performRaytracing on EVERY pixel, using the correct associated ray. 
+//'r' calls the function performRaytracing on EVERY pixel, using the correct associated ray.
 //    It then stores the result in an image "result.ppm".
-//    Initially, this function is fast (performRaytracing simply returns 
-//    the target of the ray - see the code above), but once you replaced 
-//    this function and raytracing is in place, it might take a 
+//    Initially, this function is fast (performRaytracing simply returns
+//    the target of the ray - see the code above), but once you replaced
+//    this function and raytracing is in place, it might take a
 //    while to complete...
 void yourKeyboardFunc(char t, int x, int y, const Vec3Df & rayOrigin, const Vec3Df & rayDestination)
 {
@@ -136,15 +205,15 @@ void yourKeyboardFunc(char t, int x, int y, const Vec3Df & rayOrigin, const Vec3
 	//here, as an example, I use the ray to fill in the values for my upper global ray variable
 	//I use these variables in the debugDraw function to draw the corresponding ray.
 	//try it: Press a key, move the camera, see the ray that was launched as a line.
-	testRayOrigin=rayOrigin;	
+	testRayOrigin=rayOrigin;
 	testRayDestination=rayDestination;
-	
+
 	// do here, whatever you want with the keyboard input t.
-	
+
 	//...
-	
-	
-	std::cout<<t<<" pressed! The mouse was in location "<<x<<","<<y<<"!"<<std::endl;	
+
+
+	std::cout<<t<<" pressed! The mouse was in location "<<x<<","<<y<<"!"<<std::endl;
 }
 
 Vec3Df intersectionWithPlane(const Vec3Df & planeNormal, Vec3Df & planePoint)
